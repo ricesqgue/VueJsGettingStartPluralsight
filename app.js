3cb8d90a50c5c992cff98cf069ccde4930241797
Ricardo Esqueda <ricardoeg66@gmail.com>
2018-01-23 20:53:57 -0600

    Getting start with Vue JS

---
+++ app.js
@@ -0,0 +1,21 @@
+var growler = new Vue({
+    el: '#growler',
+    data: {
+        appName: 'Glower',
+        appLink: '<a href="/">Glower</a>',
+        imgLogo: 'https://bitso.com/assets/images/b/logo_white.svg',
+        color: "#FF0000",
+        cssColor: {
+            color: "#FF0000"
+        },
+        cssFont: {
+            fontSize: "20pt"
+        },
+        isOnline: true,
+        checkboxes: [],
+        myNumber: 0
+        
+
+    
+    }
+});
